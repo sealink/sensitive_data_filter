a131d34f0851082856a3ae27f8f077abdcce8962
Alessandro Berardi <berardialessandro@gmail.com>
2016-12-07 17:45:05 +1030

    Replaces custom logic with facets/hash/collate

--- lib/sensitive_data_filter/middleware/parameter_scanner.rb
+++ lib/sensitive_data_filter/middleware/parameter_scanner.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
+require 'facets/hash/collate'
+
 module SensitiveDataFilter
   module Middleware
     class ParameterScanner
@@ -7,14 +9,8 @@ module SensitiveDataFilter
         @original_params = @env_parser.query_params.values + @env_parser.body_params.values
       end
 
-      # TODO: Use facets/hash/collate
       def matches
-        matches = scans.map(&:matches)
-        matches.flat_map(&:keys).uniq.map.with_object({}) { |type, combined_matches|
-          combined_matches[type] = matches.map { |type_matches|
-            type_matches.fetch(type, [])
-          }.inject(:+)
-        }
+        scans.map(&:matches).inject(:collate)
       end
 
       def sensitive_data?
