b42a49a1612097d7662027e2587079747e8f86b4
Grant Colegate <blaknite@thelanbox.com.au>
2016-12-08 18:15:53 +1030

    EnvFilter now initializes all dependencies on initialize
    Abstraction can be reduced resulting in a clear definition of the objects concern during initialization.

--- lib/sensitive_data_filter/middleware/env_filter.rb
+++ lib/sensitive_data_filter/middleware/env_filter.rb
@@ -4,11 +4,15 @@ require 'facets/kernel/present'
 module SensitiveDataFilter
   module Middleware
     class EnvFilter
+      attr_reader :occurrence
+
       def initialize(env)
         @env = env
-        @original_env_parser = EnvParser.new env
+        @original_env_parser = EnvParser.new(env)
+        @scanner = ParameterScanner.new(@original_env_parser)
         @filtered_env_parser = @original_env_parser.copy
         @filtered_env_parser.mask! if @scanner.sensitive_data?
+        @occurrence = build_occurrence
       end
 
       def filtered_env
@@ -19,15 +23,12 @@ module SensitiveDataFilter
         occurrence.present?
       end
 
-      def occurrence
-        return nil unless scanner.sensitive_data?
-        @occurrence ||= Occurrence.new(@original_env_parser, @filtered_env_parser, scanner.matches)
-      end
-
       private
 
-      def scanner
-        @scanner ||= ParameterScanner.new @original_env_parser
+      def build_occurrence
+        if @scanner.sensitive_data?
+          Occurrence.new(@original_env_parser, @filtered_env_parser, @scanner.matches)
+        end
       end
     end
   end
