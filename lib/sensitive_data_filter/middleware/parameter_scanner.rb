c426a24a7fb0444a785845c1a64ed3029debce09
Grant Colegate <blaknite@thelanbox.com.au>
2016-12-08 18:13:27 +1030

    ParameterScanner now scans on initialize
    PrameterScanner.new is immediately followed by a call to sensitive_data? meaning there is no reason to lazy-load the scans. They’re now just done on initialize.

--- lib/sensitive_data_filter/middleware/parameter_scanner.rb
+++ lib/sensitive_data_filter/middleware/parameter_scanner.rb
@@ -5,22 +5,17 @@ module SensitiveDataFilter
   module Middleware
     class ParameterScanner
       def initialize(env_parser)
-        @env_parser      = env_parser
-        @original_params = @env_parser.query_params.values + @env_parser.body_params.values
+        @env_parser = env_parser
+        @params     = @env_parser.query_params.values + @env_parser.body_params.values
+        @scans      = @params.map { |value| SensitiveDataFilter::Scan.new(value) }
       end
 
       def matches
-        scans.map(&:matches).inject(:collate)
+        @scans.map(&:matches).inject(:collate)
       end
 
       def sensitive_data?
-        scans.any?(&:matches?)
-      end
-
-      private
-
-      def scans
-        @scans ||= @original_params.map { |value| SensitiveDataFilter::Scan.new(value) }
+        @scans.any?(&:matches?)
       end
     end
   end
