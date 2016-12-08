11a6b41587e523771dd46aed6b354ee7061cb02a
Alessandro Berardi <berardialessandro@gmail.com>
2016-12-08 16:21:48 +1030

    Adds coverage-kit gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,31 +1,5 @@
 # frozen_string_literal: true
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
-MINIMUM_COVERAGE = 100
-
-unless ENV['COVERAGE'] == 'off'
-  require 'simplecov'
-  require 'simplecov-rcov'
-  require 'coveralls'
-  Coveralls.wear!
-
-  SimpleCov.formatters = [
-    SimpleCov::Formatter::RcovFormatter,
-    Coveralls::SimpleCov::Formatter
-  ]
-  SimpleCov.start do
-    add_filter '/vendor/'
-    add_filter '/spec/'
-    add_group 'lib', 'lib'
-  end
-  SimpleCov.at_exit do
-    SimpleCov.result.format!
-    percent = SimpleCov.result.covered_percent
-    unless percent >= MINIMUM_COVERAGE
-      puts "Coverage must be above #{MINIMUM_COVERAGE}%. It is #{format('%.2f', percent)}%"
-      Kernel.exit(1)
-    end
-  end
-end
-
+require 'support/coverage_loader'
 require 'sensitive_data_filter'
